925a58692450f5811fdff29edf887cc538ecd0da
Luis Augusto Peña Pereira <lpenap@gmail.com>
2018-11-20 11:31:39 -0400

    Add docker and environment config
    Develop branch was made again from 4.9.8 tag so configuration
    files and docker files are added again

---
+++ wp-config-env-template.php
@@ -0,0 +1,43 @@
+<?php
+
+/**#@+
+ * This is a template for defining environment-specific wordpress configuration.
+ * files needed:
+ * wp-config-local.php  for local environment.
+ * wp-config-dev.php    for development environment.
+ * wp-config.prod.php   for production environment.
+ */
+define( 'DB_NAME', '' );
+
+/** MySQL database username */
+define( 'DB_USER', '' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', '' );
+
+/** MySQL hostname */
+define( 'DB_HOST', '' );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', '' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '' );
+define( 'SECURE_AUTH_KEY',  '' );
+define( 'LOGGED_IN_KEY',    '' );
+define( 'NONCE_KEY',        '' );
+define( 'AUTH_SALT',        '' );
+define( 'SECURE_AUTH_SALT', '' );
+define( 'LOGGED_IN_SALT',   '' );
+define( 'NONCE_SALT',       '' );
---
+++ wp-config.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+* Define type of server
+*
+* Depending on the type other stuff can be configured
+* Note: Define them all, don't skip one if other is already defined
+*/
+
+/** Absolute path to the WordPress directory. */
+if (! defined('ABSPATH')) {
+    define('ABSPATH', dirname(__FILE__) . '/');
+}
+
+define('WP_CREDENTIALS_PATH', ABSPATH); // cache it for multiple use
+define('WP_SERVER_LOCAL', file_exists(WP_CREDENTIALS_PATH . 'wp-config-local.php'));
+define('WP_SERVER_DEV', file_exists(WP_CREDENTIALS_PATH . 'wp-config-dev.php'));
+define('WP_SERVER_PROD', file_exists(WP_CREDENTIALS_PATH . 'wp-config-prod.php'));
+
+/**
+* Load DB credentials
+*/
+
+if (WP_SERVER_LOCAL) {
+    require WP_CREDENTIALS_PATH . 'wp-config-local.php';
+} elseif (WP_SERVER_DEV) {
+    require WP_CREDENTIALS_PATH . 'p-config-dev.php';
+} else {
+    require WP_CREDENTIALS_PATH . 'wp-config-prod.php';
+}
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+ if (WP_SERVER_LOCAL || WP_SERVER_DEV) {
+     define('WP_DEBUG', true);
+     define('WP_DEBUG_LOG', true); // Stored in wp-content/debug.log
+     define('SCRIPT_DEBUG', true);
+     define('SAVEQUERIES', true);
+ } else {
+     define('WP_DEBUG', false);
+ }
+ 
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
